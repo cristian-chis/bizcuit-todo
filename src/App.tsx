5bec4a883274c27418df074a2f7ec49d2dded028
Chis George Cristian <chis.george.cristian@gmail.com>
2021-08-30 20:28:08 +0300

    Started implementing queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
+import { gql, useQuery, useMutation } from '@apollo/client';
 import './App.css';
 
+const GET_PROJECTS = gql`
+	{
+		projects {
+			id
+			name
+			description
+		}
+	}
+`;
+
+const CREATE_PROJECT = gql`
+	mutation CreateProject($name: String!, $description: String!) {
+		createProject(name: $name, description: $description) {
+			id
+			name
+		}
+	}
+`;
+
+function Projects() {
+	const { loading, error, data } = useQuery(GET_PROJECTS);
+  
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error {error.message}</p>;
+  
+  	return data.projects.map((project: any) => {
+		console.log({project});
+		return (
+			<div key={project.id}>
+				<p>
+					{project.id} - {project.name}: {project.description}
+				</p>
+			</div>
+		);
+	});
+}
+
+function FormAddProject() {
+	const nameRef = React.createRef<HTMLInputElement>();
+	const descriptionRef = React.createRef<HTMLInputElement>();
+	
+	const [createProject, { loading, error, data }] = useMutation(CREATE_PROJECT);
+
+	const handleSubmit: React.FormEventHandler = (event) => {
+		event.preventDefault();
+		createProject({ variables: { name: 'name', description: 'description' } });
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input ref={nameRef} type="text" />
+			<input ref={descriptionRef} type="text" />
+			<button disabled={loading}>Submit</button>
+		</form>
+	)
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<h1>My projects</h1>
+			<Projects />
+			<FormAddProject />
+		</>
+	);
 }
 
 export default App;
