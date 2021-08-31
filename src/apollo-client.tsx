5b586f8c0b496863aa30fbd3ed1014d30e93202b
Chis George Cristian <chis.george.cristian@gmail.com>
2021-08-31 19:37:14 +0300

    Rename Apollo client file

--- src/apollo-client.tsx
+++ src/apollo-client.tsx
@@ -2,7 +2,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
 	link: new HttpLink({ uri: 'http://localhost:4000' }),
-	cache: new InMemoryCache(),
+	cache: new InMemoryCache()
 });
 
-export { client };
+export default client;
