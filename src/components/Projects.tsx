f3594163fb629b3b4c6fad4e19a98b61ca8d1dbb
Chis George Cristian <chis.george.cristian@gmail.com>
2021-08-31 19:40:53 +0300

    Created component for displaying all projects

---
+++ src/components/Projects.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { ProjectItem, ProjectProps } from './Project';
+
+export const GET_PROJECTS = gql`
+	{
+		projects {
+			id
+			name
+			description
+		}
+	}
+`;
+
+export function Projects() {
+	const { loading, error, data } = useQuery(GET_PROJECTS);
+  
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error {error.message}</p>;
+
+  	return (
+		<table className="table">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Description</th>
+					<th>Actions</th>
+				</tr>
+			</thead>
+			<tbody>
+				{data.projects.map((project: ProjectProps) => <ProjectItem key={project.id} id={project.id} name={project.name} description={project.description} /> )}
+			</tbody>
+		</table>
+		
+	);
+}
