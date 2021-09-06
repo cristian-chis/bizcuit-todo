27f0188fdf954b39b2a7caa3cbcbdffb30cf2dff
Chis George Cristian <chis.george.cristian@gmail.com>
2021-09-06 18:38:44 +0300

    Switched from state to ref + added basic validation

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -28,9 +28,11 @@ export interface ProjectProps extends RouteComponentProps {
 }
 
 export const Project = withRouter(({ match, location, history, ...props }: ProjectProps) => {
-	const [name, setName] = React.useState(props.name);
-	const [description, setDescription] = React.useState(props.description);
 	const [isUpdating, setIsUpdating] = React.useState(false);
+	const [isErrored, setIsErrored] = React.useState(false);
+
+	const nameInputRef = React.createRef<HTMLInputElement>();
+	const descriptionInputRef = React.createRef<HTMLTextAreaElement>();
 
 	const [updateProject] = useMutation(UPDATE_PROJECT, {
 		refetchQueries: [{ query: GET_PROJECTS }]
@@ -40,35 +42,62 @@ export const Project = withRouter(({ match, location, history, ...props }: Proje
 		refetchQueries: [{ query: GET_PROJECTS }]
 	});
 
+	const handleUpdate = () => {
+		// optional chaining not compiling (using caveman JS)
+		const nameInputValue = nameInputRef.current && nameInputRef.current.value
+			? nameInputRef.current.value
+			: null
+		const descriptionInputValue = descriptionInputRef.current && descriptionInputRef.current.value
+			? descriptionInputRef.current.value
+			: null
+
+		if (nameInputValue && descriptionInputValue) {
+			updateProject({ variables: { id: props.id, name: nameInputValue, description: descriptionInputValue } });
+			setIsUpdating(false);
+		} else {
+			setIsErrored(true);
+		}
+	}
+
+	React.useEffect(() => {
+		if (!isUpdating) {
+			setIsErrored(false);
+		}
+	}, [isUpdating]);
+
 	return (
-		<tr className="table__item">
-			<td className="table__item-cell">{
-				isUpdating
-					? <input type="text" placeholder="Project name" value={name} onChange={(event) => setName(event.target.value)} required />
-					: name
-			}</td>
-			<td className="table__item-cell">{
-				isUpdating
-					? <textarea placeholder="Project description" value={description} onChange={(event) => setDescription(event.target.value)} required />
-					: description
-			}</td>
-			<td className="table__item-cell table__item-cell-actions">
-				<div style={{ display: 'flex', justifyContent: 'center' }}>{
+		<>
+			{isErrored &&
+				<tr className="table__item">
+					<td colSpan={3} className="error">One or more fields could not be validated. Make sure you set a value for both fields. Please check and submit again.</td>
+				</tr>
+			}
+			<tr className="table__item">
+				<td className="table__item-cell">{
 					isUpdating
-						? <>
-							<button className="table__action-button table__action-button--update" onClick={(event) => {
-								updateProject({ variables: { id: props.id, name, description } });
-								setIsUpdating(false);
-							}}>Update</button>
-							<button className="table__action-button table__action-button--cancel" onClick={(event) => setIsUpdating(false)}>Cancel</button>
-						</>
-						: <>
-							<button className="table__action-button table__action-button--view" onClick={(event) => history.push(`/project/${props.id}`) }>View</button>
-							<button className="table__action-button table__action-button--update" onClick={(event) => setIsUpdating(true)}>Edit</button>
-							<button className="table__action-button table__action-button--delete" onClick={(event) => deleteProject({ variables: { id: props.id } })}>Delete</button>
-						</>
-				}</div>
-			</td>
-		</tr>
+						? <input ref={nameInputRef} type="text" placeholder="Project name" defaultValue={props.name} required />
+						: props.name
+				}</td>
+				<td className="table__item-cell">{
+					isUpdating
+						? <textarea ref={descriptionInputRef} placeholder="Project description" defaultValue={props.description} required />
+						: props.description
+				}</td>
+				<td className="table__item-cell table__item-cell-actions">
+					<div style={{ display: 'flex', justifyContent: 'center' }}>{
+						isUpdating
+							? <>
+								<button className="table__action-button table__action-button--update" onClick={(event) => { handleUpdate(); }}>Update</button>
+								<button className="table__action-button table__action-button--cancel" onClick={(event) => setIsUpdating(false)}>Cancel</button>
+							</>
+							: <>
+								<button className="table__action-button table__action-button--view" onClick={(event) => history.push(`/project/${props.id}`) }>View</button>
+								<button className="table__action-button table__action-button--update" onClick={(event) => setIsUpdating(true)}>Edit</button>
+								<button className="table__action-button table__action-button--delete" onClick={(event) => deleteProject({ variables: { id: props.id } })}>Delete</button>
+							</>
+					}</div>
+				</td>
+			</tr>
+		</>
 	);
 });
